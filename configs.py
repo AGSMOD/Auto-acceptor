b165c04d4c9adc1c2d1be8c59feac8750686f417
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-11-10 16:39:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "57b85f72104db3f08f9795b0410eb556")
     BOT_TOKEN = getenv("BOT_TOKEN", "6835125869:AAGWrHgrJezV3DZYVHz-JA4izYAIISdHLGc")
     FSUB = getenv("FSUB", "Agsmods")
-    CHID = int(getenv("CHID", "-1002091999754)
+    CHID = int(getenv("CHID", "-1002091999754")
     SUDO = list(map(int, getenv("SUDO", "6968528769").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Gali:Gali@cluster0.qm9j1ag.mongodb.net/?retryWrites=true&w=majority")
     
