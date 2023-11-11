32a5a84458c513c5574be9c91a105e67db0fda64
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-11-11 09:48:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "20919286"))
     API_HASH = getenv("API_HASH", "57b85f72104db3f08f9795b0410eb556")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6835125869:AAGWrHgrJezV3DZYVHz-JA4izYAIISdHLGc")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6835125869:AAG2b1HTuDy9Ixq_wawdEyjhyeLelcn8xx0")
     FSUB = getenv("FSUB", "Agsmods")
     CHID = int(getenv("CHID", "-1002091999754")
     SUDO = int(getenv("SUDO", "6968528769").split()))
