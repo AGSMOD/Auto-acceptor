a2769c2b1eedaddc8606c919ac6990b325bed072
AGSMOD <149931558+AGSMOD@users.noreply.github.com>
2023-11-11 10:01:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ app = Client(
     "approver",
     api_id=cfg.API_ID,
     api_hash=cfg.API_HASH,
-    bot_token=cfg.BOT_TOKEN
+    bot_token=cfg.BOT_TOKEN,
+    mongo_uri=cfg.MONGO_URI
 )
 
 gif = [
